cf9320ee17221ea1579d1e2322a2688740ee969f
Kajetan Pawliszyn <kajetan.pawliszyn@gmail.com>
2020-11-17 00:22:59 +0100

    Add database

--- company_blog/__init__.py
+++ company_blog/__init__.py
@@ -1,8 +1,23 @@
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 
+####################################
+#          DATABASE SETUP          #
+####################################
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+db = SQLAlchemy(app)
+Migrate(app, db)
+
+####################################
+#       REGISTER BLUEPRINTS        #
+####################################
 from company_blog.core.views import core
 from company_blog.error_pages.handlers import error_pages
 
---
+++ company_blog/models.py
@@ -0,0 +1,9 @@
+from company_blog import db
+
+
+class User(db.Model):
+    pass
+
+
+class BlogPost(db.Model):
+    pass
