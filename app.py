157e50626aaa19a9a22421e05e34a30d31a0797f
Kajetan Pawliszyn <kajetan.pawliszyn@gmail.com>
2020-11-16 23:56:46 +0100

    Add file structure

--- app.py
+++ app.py
@@ -1,11 +1,4 @@
-from myproject import app
-from flask import render_template
-
-
-@app.route('/')
-def index():
-    return render_template('home.html')
-
+from company_blog import app
 
 if __name__ == '__main__':
     app.run(debug=True)
