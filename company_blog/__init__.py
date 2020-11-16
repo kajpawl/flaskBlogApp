bfde28e14f64e642c714376710f97d7965d574c2
Kajetan Pawliszyn <kajetan.pawliszyn@gmail.com>
2020-11-17 00:14:38 +0100

    Add error pages

--- company_blog/__init__.py
+++ company_blog/__init__.py
@@ -4,4 +4,7 @@ app = Flask(__name__)
 
 
 from company_blog.core.views import core
+from company_blog.error_pages.handlers import error_pages
+
 app.register_blueprint(core)
+app.register_blueprint(error_pages)
