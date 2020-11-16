5e6938752076dc8b3f35048ad71147ca799f7ef9
Kajetan Pawliszyn <kajetan.pawliszyn@gmail.com>
2020-11-17 00:40:18 +0100

    Add user and post models

--- company_blog/__init__.py
+++ company_blog/__init__.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
 
@@ -14,6 +15,14 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 Migrate(app, db)
+
+####################################
+#           LOGIN CONFIGS          #
+####################################
+login_manager = LoginManager()
+
+login_manager.init_app(app)
+login_manager.login_view = 'users.login'
 
 ####################################
 #       REGISTER BLUEPRINTS        #
