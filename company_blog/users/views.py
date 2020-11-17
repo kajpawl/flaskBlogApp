96d5615701c533b40606a7cbd8ee441badace47e
Kajetan Pawliszyn <kajetan.pawliszyn@gmail.com>
2020-11-18 00:48:28 +0100

    Add next user views

--- company_blog/users/views.py
+++ company_blog/users/views.py
@@ -8,12 +8,6 @@ from company_blog.users.picture_handler import add_profile_pic
 users = Blueprint('user', __name__)
 
 
-@users.route('/logout')
-def logout():
-    logout_user()
-    return redirect(url_for('core.index'))
-
-
 @users.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
@@ -51,4 +45,45 @@ def login():
             return redirect(next)
 
     return render_template('login.html', form=form)
+
+
+@users.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('core.index'))
+
+
+@users.route('/account', methods=['GET', 'POST'])
+@login_required
+def account():
+    form = UpdateUserForm()
+
+    if form.validate_on_submit():
+        if form.picture.data:
+            username = current_user.username
+            pic = add_profile_pic(form.picture.data, username)
+            current_user.profile_image = pic
+
+        current_user.username = form.user.data
+        current_user.email = form.email.data
+
+        db.session.commit()
+
+        flash('User account updated!')
+        return redirect(url_for('users.account'))
+
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+
+    profile_image = url_for('static', filename='profile_pics/' + current_user.profile_image)
+    return render_template('account.html', profile_image=profile_image, form=form)
+
+
+@users.route('/<username>')
+def user_posts(username):
+    page = request.args.get('page', 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    blog_posts = BlogPost.query.filter_by(author=user).order_by(BlogPost.date.desc()).paginate(page=page, per_page=5)
+    return render_template('user_blog_posts.html', blog_posts, user=user)
 
