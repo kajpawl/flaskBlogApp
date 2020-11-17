fb31aff4d74bd3569f018ad424a4242ed9532dad
Kajetan Pawliszyn <kajetan.pawliszyn@gmail.com>
2020-11-18 00:25:14 +0100

    Add user views - login, register

--- company_blog/__init__.py
+++ company_blog/__init__.py
@@ -28,7 +28,9 @@ login_manager.login_view = 'users.login'
 #       REGISTER BLUEPRINTS        #
 ####################################
 from company_blog.core.views import core
+from company_blog.users.views import users
 from company_blog.error_pages.handlers import error_pages
 
 app.register_blueprint(core)
+app.register_blueprint(users)
 app.register_blueprint(error_pages)
---
+++ company_blog/users/views.py
@@ -0,0 +1,54 @@
+from flask import render_template, url_for, flash, redirect, request, Blueprint
+from flask_login import login_user, current_user, logout_user, login_required
+from company_blog import db
+from company_blog.models import User, BlogPost
+from company_blog.users.forms import RegistrationForm, LoginForm, UpdateUserForm
+from company_blog.users.picture_handler import add_profile_pic
+
+users = Blueprint('user', __name__)
+
+
+@users.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('core.index'))
+
+
+@users.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+
+    if form.validate_on_submit():
+        user = User(email=form.email.data,
+                    username=form.username.data,
+                    password=form.password.data)
+
+        db.session.add(user)
+        db.session.commit()
+
+        flash('Thank you for registering!')
+        return redirect(url_for('users.login'))
+
+    return render_template('register.html', form=form)
+
+
+@users.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+
+        if user is not None and user.check_password(form.password.data):
+            login_user(user)
+            flash('Login success!')
+
+            next = request.args.get('next')
+
+            if next is None or not next[0] == '/':
+                next = url_for('core.index')
+
+            return redirect(next)
+
+    return render_template('login.html', form=form)
+
